ef9233e088f6b769589d2d00051c20b4434a92a1
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2023-11-24 21:43:10 +0100

    updating CI

--- include/Register.hpp
+++ include/Register.hpp
@@ -20,20 +20,27 @@ struct WRITEONLY
 };
 
 struct READWRITE
-  : public READONLY
-  , public WRITEONLY
+    : READONLY
+      , WRITEONLY
 {
 };
 
 struct BitType
 {
-    consteval BitType() : type("N/A")
-    {}
-    consteval explicit BitType(const char *data) : type{ data, std::char_traits<char>::length(data) }
-    {}
+    consteval BitType()
+        : type("N/A")
+    {
+    }
+
+    consteval explicit BitType(const char *data)
+        : type{ data, std::char_traits<char>::length(data) }
+    {
+    }
 #ifdef USE_FMT
-    explicit BitType(std::string_view data) : type{ data }
-    {}
+    explicit BitType(const std::string_view data)
+        : type{ data }
+    {
+    }
 #endif
 
     std::string_view type;
@@ -41,30 +48,35 @@ struct BitType
 
 struct BitName
 {
-    consteval BitName() : name("Reserved")
-    {}
-    consteval explicit BitName(const char *data) : name{ data, std::char_traits<char>::length(data) }
-    {}
+    consteval BitName()
+        : name("Reserved")
+    {
+    }
+
+    consteval explicit BitName(const char *data)
+        : name{ data, std::char_traits<char>::length(data) }
+    {
+    }
 
     std::string_view name;
 };
 
 namespace details {
 template<typename T>
-concept WriteConcept = std::is_same_v<T, READWRITE> || std::is_same_v<T, WRITEONLY>;
+concept WriteConcept = std::is_same_v<std::remove_cvref_t<T>, READWRITE> || std::is_same_v<std::remove_cvref_t<T>, WRITEONLY>;
 
 template<typename T>
-concept ReadConcept = std::is_same_v<T, READWRITE> || std::is_same_v<T, READONLY>;
+concept ReadConcept = std::is_same_v<std::remove_cvref_t<T>, READWRITE> || std::is_same_v<std::remove_cvref_t<T>, READONLY>;
 
 template<typename T, typename U>
-concept NotSameType = !std::is_same_v<T, U> && std::is_class_v<T> && std::is_class_v<U>;
+concept NotSameType = !std::is_same_v<std::remove_cvref_t<T>, U> && std::is_class_v<std::remove_cvref_t<T>> && std::is_class_v<U>;
 
 template<typename Type>
-[[nodiscard]] constexpr auto getMask(std::size_t bitOffset, std::size_t bitWidth)
+[[nodiscard]] constexpr auto getMask(const std::size_t bitOffset, const std::size_t bitWidth)
 {
-    Type mask = 0u;
+    Type mask = 0U;
     for (std::size_t i = 0; i < bitWidth; i++) {
-        mask |= static_cast<Type>(1 << (bitOffset + i));
+        mask |= static_cast<Type>(1U << (bitOffset + i));
     }
     return mask;
 }
@@ -73,25 +85,28 @@ template<unsigned N>
 struct FixedString
 {
     std::array<char, N + 1> buf{};
+
     consteval FixedString(char const *input)
     {
         for (unsigned i = 0; i != N; ++i) {
             buf[i] = input[i];
         }
     }
+
     [[nodiscard]] constexpr operator char const *() const
     {
         return buf.data();
     }
 };
+
 template<unsigned N>
 FixedString(const char (&)[N]) -> FixedString<N - 1>;
 
 #ifdef USE_FMT
 using namespace std::string_view_literals;
-static std::map<std::type_index, BitType> typeMap = { { typeid(READONLY), BitType{ "R"sv } },
-    { typeid(WRITEONLY), BitType{ "W"sv } },
-    { typeid(READWRITE), BitType{ "R/W"sv } } };
+static std::map<std::type_index, BitType> TypeMap = { { typeid(READONLY), BitType{ "R"sv } },
+                                                      { typeid(WRITEONLY), BitType{ "W"sv } },
+                                                      { typeid(READWRITE), BitType{ "R/W"sv } } };
 #endif
 
 }// namespace details
@@ -100,8 +115,8 @@ static std::map<std::type_index, BitType> typeMap = { { typeid(READONLY), BitTyp
 template<typename T, size_t BitOffset, size_t BitWidth, details::FixedString Name, typename FieldType = READWRITE>
 struct BitField
 {
-    static constexpr std::size_t bitOffset = BitOffset;
-    static constexpr std::size_t bitWidth = BitWidth;
+    static constexpr auto bitOffset = BitOffset;
+    static constexpr auto bitWidth = BitWidth;
     static constexpr auto mask = details::getMask<std::uint32_t>(BitOffset, BitWidth);
     static constexpr char const *name = Name;
     constexpr static FieldType Type{};
@@ -133,8 +148,11 @@ struct EnumeratedField
 
 struct BitInfo
 {
-    explicit BitInfo(size_t width = 0, BitType bitType = {}, BitName bitName = {}) : bitWidth(width), type(bitType.type), name(bitName.name)
-    {}
+    explicit BitInfo(const size_t width = 0, const BitType bitType = {}, const BitName bitName = {})
+        : bitWidth(width), type(bitType.type), name(bitName.name)
+    {
+    }
+
     size_t bitWidth;
     std::string_view type;
     std::string_view name;
@@ -144,64 +162,74 @@ struct BitInfo
 template<typename RegisterWidth, std::uintptr_t BaseAddress, RegisterWidth ResetValue, typename RegisterType, details::FixedString Name, typename... Fields>
 class Register
 {
-  public:
+public:
     Register &operator=(RegisterWidth bitMask)
     {
         write<RegisterType>(bitMask);
+        return *this;
     }
 
+    template<typename T = RegisterType>
+        requires details::ReadConcept<T>
     RegisterWidth operator()(const RegisterWidth mask) const
     {
-        return read<RegisterType>(mask);
+        return *rawPtr & mask;
     }
 
     template<typename Value>
         requires details::ReadConcept<RegisterType>
     [[nodiscard]] RegisterWidth read() const
     {
-        return read<RegisterType>(Value::mask) >> Value::bitOffset;
+        static_assert((std::is_same_v<std::remove_cvref_t<Value>, std::remove_cvref_t<Fields>> || ...), "Bitfield not defined for register");
+        return (*rawPtr & Value::mask) >> Value::bitOffset;
     }
 
+    template<typename T = RegisterType>
+        requires details::ReadConcept<T>
     [[nodiscard]] RegisterWidth operator()() const
     {
-        return read<RegisterType>(std::numeric_limits<int>::max());
+        return *rawPtr & std::numeric_limits<int>::max();
     }
 
+    template<typename T = RegisterType>
+        requires details::WriteConcept<T>
     void operator|=(RegisterWidth bitMask)
     {
-        orAssign<RegisterType>(bitMask);
+        *rawPtr |= bitMask;
     }
 
     template<typename Field>
+        requires details::WriteConcept<decltype(Field::Type)>
     void operator|=([[maybe_unused]] Field field)
     {
-        static_assert((std::is_same_v<const Field, Fields> || ...), "Bitfield not defined for register");
-        static_assert(!std::is_same_v<decltype(field.Type), READONLY>, "bitfield is readonly");
+        static_assert((std::is_same_v<std::remove_cvref_t<Field>, std::remove_cvref_t<Fields>> || ...), "Bitfield not defined for register");
         static_assert(field.bitWidth == 1U, "bitWidth is greater 1, please call write operation!");
-        orAssign<RegisterType>(static_cast<RegisterWidth>(Field::mask));
+        *rawPtr |= static_cast<RegisterWidth>(Field::mask);
     }
 
+    template<typename Field>
+        requires details::WriteConcept<decltype(Field::Type)>
     void operator&=(RegisterWidth bitMask)
     {
-        andAssign<RegisterType>(bitMask);
+        *rawPtr &= bitMask;
     }
 
     template<typename Field>
+        requires details::WriteConcept<decltype(Field::Type)>
     void operator&=([[maybe_unused]] Field field)
     {
-        static_assert((std::is_same_v<const Field, Fields> || ...), "Bitfield not defined for register");
-        static_assert(!std::is_same_v<decltype(field.Type), READONLY>, "bitfield is readonly");
+        static_assert((std::is_same_v<std::remove_cvref_t<Field>, std::remove_cvref_t<Fields>> || ...), "Bitfield not defined for register");
         static_assert(field.bitWidth == 1U, "bitWidth is greater 1, please call write operation!");
-        orAssign<RegisterType>(static_cast<RegisterWidth>(Field::mask));
+        *rawPtr &= static_cast<RegisterWidth>(Field::mask);
     }
 
     template<typename Field>
+        requires details::WriteConcept<decltype(Field::Type)>
     void write(Field field, RegisterWidth value)
     {
-        static_assert((std::is_same_v<const Field, Fields> || ...), "Bitfield not defined for register");
-        static_assert(!std::is_same_v<decltype(field.Type), READONLY>, "bitfield is readonly");
+        static_assert((std::is_same_v<std::remove_cvref_t<Field>, std::remove_cvref_t<Fields>> || ...), "Bitfield not defined for register");
         static_assert(field.bitWidth > 1U, "bitWidth is equal to 1, use | or & operator!");
-        orAssign<RegisterType>(static_cast<RegisterWidth>((value << field.bitOffset) & field.mask));
+        *rawPtr |= static_cast<RegisterWidth>((value << field.bitOffset) & field.mask);
     }
 
     void reset()
@@ -220,12 +248,12 @@ class Register
             fmt::print("Register name: {:^12}\n", name);
             std::array<BitInfo, std::numeric_limits<RegisterWidth>::digits> bitInfos;
             std::apply(
-              [&](auto &...) {
-                  ((bitInfos[Fields::bitOffset] = BitInfo{ Fields::bitWidth, details::typeMap[typeid(Fields::Type)], BitName{ Fields::name } }), ...);
-              },
-              bitInfos);
+                [&](auto &...) {
+                    ((bitInfos[Fields::bitOffset] = BitInfo{ Fields::bitWidth, details::TypeMap[typeid(Fields::Type)], BitName{ Fields::name } }), ...);
+                },
+                bitInfos);
             size_t offset = 0;
-            for (auto iter = bitInfos.begin(); iter != bitInfos.end(); iter++, offset++) {
+            for (auto iter = bitInfos.begin(); iter != bitInfos.end(); ++iter, offset++) {
                 fmt::print(horizontalLine, "");
                 if (iter->bitWidth > 1) {
                     fmt::print(bitContentMultipleWidth, iter->name, iter->type, offset, offset + iter->bitWidth - 1);
@@ -233,8 +261,8 @@ class Register
                     fmt::print(bitContentSingleWidth, iter->name, iter->type, offset);
                 }
                 for (size_t i = 1; i < iter->bitWidth; i++) {
-                    offset++;
-                    iter++;
+                    ++offset;
+                    ++iter;
                     fmt::print("|{0:^40} |\n", "");
                 }
             }
@@ -243,40 +271,13 @@ class Register
 #endif
     }
 
-  private:
-    template<details::WriteConcept T>
-    void write(RegisterWidth bitMask)
-    {
-        *rawPtr = bitMask;
-    }
-
-    template<details::ReadConcept T>
-    RegisterWidth read(RegisterWidth mask) const
-    {
-        return *rawPtr & mask;
-    }
-    template<details::ReadConcept T>
-    RegisterWidth read() const
-    {
-        return *rawPtr;
-    }
-
-    template<details::WriteConcept T>
-    void orAssign(RegisterWidth bitMask)
-    {
-        *rawPtr |= bitMask;
-    }
-
-    template<details::WriteConcept T>
-    void andAssign(RegisterWidth bitMask)
-    {
-        *rawPtr &= bitMask;
-    }
-
+private:
+#ifdef USE_FMT
     RegisterWidth raw = { ResetValue };
     RegisterWidth *rawPtr = reinterpret_cast<RegisterWidth *>(&raw);
 
-#ifdef USE_FMT
     static constexpr char const *name = Name;
+#else
+    RegisterWidth *rawPtr = BaseAddress;
 #endif
 };
