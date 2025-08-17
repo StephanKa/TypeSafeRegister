3b29f731c7c2ca44c0db2f68db6d6f9eb410e649
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2025-08-17 19:22:22 +0200

    updated conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,25 @@
 """Conanfile module for this project."""
-import conans.model.requires
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain
 
 
 class ProjectConan(ConanFile):
-    """This class defines the handling for different setups."""
-
     settings = 'os', 'compiler', 'build_type', 'arch'
     default_options = {'fmt/*:header_only': True}
     generators = 'CMakeDeps', 'CMakeToolchain'
 
-    def configure(self):
-        """Override the configure method and defines different requirements for different architectures."""
+    def requirements(self):
+        self.requires('fmt/11.2.0')
         if self.settings.get_safe('arch') == 'armv7':
-            self.requires = conans.model.requires.Requirements(['fmt/11.0.2'])
-        else:
-            self.requires = conans.model.requires.Requirements(['catch2/3.7.0', 'fmt/11.0.2'])
+            return
+
+        self.requires('catch2/3.9.1')
+
+    def configure(self):
+        cmake = CMakeToolchain(self)
+        cmake.user_presets_path = None
 
     def build(self):
-        """Override the build method."""
         cmake = CMakeToolchain(self)
         cmake.configure()
         cmake.build()
