9fe7a88502b5a73bebe4cf2dcf4614a8a101fa94
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2026-08-01 19:49:14 +0200

    updated Conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,27 @@
 """Conanfile module for this project."""
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class ProjectConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
-    default_options = {'fmt/*:header_only': True}
     generators = 'CMakeDeps', 'CMakeToolchain'
 
-    def _uses_legacy_format(self):
+    def layout(self):
+        cmake_layout(self)
+
+    def validate(self):
         cppstd = self.settings.get_safe('compiler.cppstd')
-        return cppstd is not None and int(str(cppstd).removeprefix('gnu')) < 20
+        if cppstd is not None and int(str(cppstd).removeprefix('gnu')) < 23:
+            raise ConanInvalidConfiguration('TypeSafeRegister requires C++23.')
 
-    def requirements(self):
-        if self._uses_legacy_format():
-            self.requires('fmt/11.2.0')
-        if self.settings.get_safe('arch') == 'armv7':
-            return
-
-        self.requires('catch2/3.9.1')
-
-    def configure(self):
-        cmake = CMakeToolchain(self)
-        cmake.user_presets_path = None
+    def build_requirements(self):
+        if can_run(self):
+            self.test_requires('catch2/3.9.1')
 
     def build(self):
-        cmake = CMakeToolchain(self)
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
